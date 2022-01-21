26a90de303e9020699a6c1761dc26e5fa4891522
Lukas590 <eldrego123@outlook.com>
2022-01-21 15:19:27 +0100

    VS
    goda VS onda för grejens skull

---
+++ goda+onda.py
@@ -0,0 +1,7 @@
+import random
+
+goda = ["spiderman", "ironman", "captain america"]
+onda = ["doctordoom", "joker", "loki"]
+
+
+print(goda[random.randrange(0,2)] + " VS " + onda[random.randrange(0,2)])
