1b46ae1c4fc219533f30ae89fd02193b8c9d7695
Lukas590 <eldrego123@outlook.com>
2022-05-16 11:06:58 +0200

    added better version of google
    thank me later world.

--- projekt/Magic 8 ball.py
+++ projekt/Magic 8 ball.py
@@ -1,7 +1,12 @@
 import random
 
+import requests, lxml
+from bs4 import BeautifulSoup
 from playsound import playsound
 import cv2
+
+
+
 image = cv2.imread("R.jpg")
 cv2.imshow("magic 8 ball", image)
 cv2.waitKey(1000)
@@ -9,6 +14,14 @@ cv2.destroyAllWindows()
 
 def magic8ball():
     response = input("(Press 'any key' for answer and 'quit' to exit)\nWhat is your question?\n")
+
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'}
+    r = requests.get('https://www.google.com/search?q=' + response, headers=headers)
+    soup = BeautifulSoup(r.text, 'lxml')
+
+    result = soup.find('div', class_='Z0LcW')
+
+
     Eightball_answers = [
         "It is certain",
         "Outlook good",
@@ -34,7 +47,11 @@ def magic8ball():
         cv2.waitKey(1000)
         cv2.destroyAllWindows()
         playsound('magic-chime-02.wav')
-        print(random.choice(Eightball_answers), "\n")
+        if result == None:
+            print(random.choice(Eightball_answers), "\n")
+        else:
+            print(result.text)
+
         magic8ball()
 
 magic8ball()
